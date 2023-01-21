3e49d2cae19f5308f8d8f090bcc770d534f1e67a
JoonMarion <marianojoao52@gmail.com>
2023-01-21 15:14:46 -0300

    fix: crud complete for all models

--- medicos/urls.py
+++ medicos/urls.py
@@ -4,15 +4,26 @@ from . import views
 app_name = 'medicos'
 
 urlpatterns = [
-    path('registro/medico/', views.medico_cadastro, name='medico_cadastro'),
-    path('registro/especialidade/', views.especialidade_cadastro, name='especialidade_cadastro'),
+    path('registro/profissional/', views.medico_cadastro, name='medico_cadastro'),
+    path('atualizar/profissional/<int:pk>', views.medico_atualizar, name='medico_atualizar'),
+    path('apagar/profissional/<int:pk>', views.medico_deletar, name='medico_deletar'),
+
+    path('registro/procedimento/', views.especialidade_cadastro, name='especialidade_cadastro'),
+    path('atualizar/procedimento/<int:pk>', views.especialidade_atualizar, name='especialidade_atualizar'),
+    path('apagar/procedimento/<int:pk>', views.especialidade_deletar, name='especialidade_deletar'),
+
     path('registro/cliente/', views.cliente_cadastro, name='cliente_cadastro'),
+    path('atualizar/cliente/<int:pk>', views.cliente_atualizar, name='cliente_atualizar'),
+    path('apagar/cliente/<int:pk>', views.cliente_deletar, name='cliente_deletar'),
+
     path('agendar/', views.agenda_cadastro, name='agendar_consulta'),
-    path('agendar/atualizar/<int:pk>/', views.agenda_atualizar, name='agendar_consulta_atualizar'),
-    path('agendar/apagar/<int:pk>/', views.agenda_deletar, name='agendar_consulta_deletar'),
+    path('agenda/atualizar/<int:pk>/', views.agenda_atualizar, name='agendar_consulta_atualizar'),
+    path('agenda/apagar/<int:pk>/', views.agenda_deletar, name='agendar_consulta_deletar'),
+
     path('minhas/consultas/', views.agenda_lista, name="agenda_lista"),
-    path('admim/lista/medicos/', views.medico_lista, name="medicos_lista"),
-    path('admim/lista/especialidades/', views.especialidade_lista, name="especialidade_lista"),
-    path('admim/lista/cliente/', views.cliente_lista, name="cliente_lista"),
+
+    path('admin/lista/profissionais/', views.medico_lista, name="medicos_lista"),
+    path('admin/lista/procedimentos/', views.especialidade_lista, name="especialidade_lista"),
+    path('admin/lista/clientes/', views.cliente_lista, name="cliente_lista"),
 
 ]
--- medicos/views.py
+++ medicos/views.py
@@ -28,12 +28,34 @@ class MedicoCreateView(LoginRequiredMixin, TestMixinIsAdmin, CreateView):
     success_url = reverse_lazy('medicos:medicos_lista')
 
 
+class MedicoUpdateView(LoginRequiredMixin, TestMixinIsAdmin, UpdateView):
+    model = Medico
+    login_url = 'accounts:login'
+    template_name = 'medicos/cadastro.html'
+    fields = ['nome', 'telefone', 'especialidade']
+    success_url = reverse_lazy('medicos:medicos_lista')
+
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        return super().form_valid(form)
+
+
 class MedicoListView(LoginRequiredMixin, TestMixinIsAdmin, ListView):
     login_url = 'accounts:login'
     template_name = 'medicos/medicos_list.html'
 
     def get_queryset(self):
         return Medico.objects.all().order_by('-pk')
+
+
+class MedicoDeleteView(LoginRequiredMixin, TestMixinIsAdmin, DeleteView):
+    model = Medico
+    success_url = reverse_lazy('medicos:medicos_lista')
+    template_name = 'form_delete.html'
+
+    def get_success_url(self):
+        messages.success(self.request, "Profissional excluído com sucesso!")
+        return reverse_lazy('medicos:medicos_lista')
 
 
 class EspecialidadeCreateView(LoginRequiredMixin, TestMixinIsAdmin, CreateView):
@@ -44,6 +66,18 @@ class EspecialidadeCreateView(LoginRequiredMixin, TestMixinIsAdmin, CreateView):
     success_url = reverse_lazy('medicos:especialidade_lista')
 
 
+class EspecialidadeUpdateView(LoginRequiredMixin, TestMixinIsAdmin, UpdateView):
+    model = Especialidade
+    login_url = 'accounts:login'
+    template_name = 'medicos/cadastro.html'
+    fields = ['nome']
+    success_url = reverse_lazy('medicos:especialidade_lista')
+
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        return super().form_valid(form)
+
+
 class EspecialidadeListView(LoginRequiredMixin, TestMixinIsAdmin, ListView):
     login_url = 'accounts:login'
     template_name = 'medicos/especialidade_list.html'
@@ -52,12 +86,44 @@ class EspecialidadeListView(LoginRequiredMixin, TestMixinIsAdmin, ListView):
         return Especialidade.objects.all().order_by('-pk')
 
 
+class EspecialidadeDeleteView(LoginRequiredMixin, TestMixinIsAdmin, DeleteView):
+    model = Especialidade
+    success_url = reverse_lazy('medicos:especialidade_lista')
+    template_name = 'form_delete.html'
+
+    def get_success_url(self):
+        messages.success(self.request, "Procedimento excluído com sucesso!")
+        return reverse_lazy('medicos:especialidade_lista')
+
+
 class ClienteCreateView(LoginRequiredMixin, TestMixinIsAdmin, CreateView):
     model = Cliente
     login_url = 'accounts:login'
     template_name = 'medicos/cadastro.html'
     fields = ['nome', 'telefone']
     success_url = reverse_lazy('medicos:cliente_lista')
+
+
+class ClienteUpdateView(LoginRequiredMixin, TestMixinIsAdmin, UpdateView):
+    model = Cliente
+    login_url = 'accounts:login'
+    template_name = 'medicos/cadastro.html'
+    fields = ['nome', 'telefone']
+    success_url = reverse_lazy('medicos:cliente_lista')
+
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        return super().form_valid(form)
+
+
+class ClienteDeleteView(LoginRequiredMixin, TestMixinIsAdmin, DeleteView):
+    model = Cliente
+    success_url = reverse_lazy('medicos:cliente_lista')
+    template_name = 'form_delete.html'
+
+    def get_success_url(self):
+        messages.success(self.request, "Cliente excluído com sucesso!")
+        return reverse_lazy('medicos:cliente_lista')
 
 
 class ClienteListView(LoginRequiredMixin, TestMixinIsAdmin, ListView):
@@ -115,11 +181,20 @@ class AgendaListView(LoginRequiredMixin, TestMixinIsAdmin, ListView):
 
 
 medico_cadastro = MedicoCreateView.as_view()
+medico_atualizar = MedicoUpdateView.as_view()
 medico_lista = MedicoListView.as_view()
+medico_deletar = MedicoDeleteView.as_view()
+
 especialidade_cadastro = EspecialidadeCreateView.as_view()
+especialidade_atualizar = EspecialidadeUpdateView.as_view()
 especialidade_lista = EspecialidadeListView.as_view()
+especialidade_deletar = EspecialidadeDeleteView.as_view()
+
 cliente_cadastro = ClienteCreateView.as_view()
+cliente_atualizar = ClienteUpdateView.as_view()
 cliente_lista = ClienteListView.as_view()
+cliente_deletar = ClienteDeleteView.as_view()
+
 agenda_cadastro = AgendaCreateView.as_view()
 agenda_atualizar = AgendaUpdateView.as_view()
 agenda_lista = AgendaListView.as_view()
