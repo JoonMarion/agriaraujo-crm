6df73fadad3a6024db115c7c4f2adabe1e112206
JoonMariano <marianojoao52@gmail.com>
2023-06-05 14:48:11 -0300

    fix: form caixa, list caixa, template recibo

--- caixa/models.py
+++ caixa/models.py
@@ -7,13 +7,12 @@ from clientes.utils import calcular_valor_total
 class Caixa(models.Model):
     TIPOS_TRANSACAO = (
         ('E', 'Entrada'),
-        ('S', 'Saída')
     )
     data = models.DateField()
     descricao = models.CharField(max_length=200)
     quantidade_kg = models.DecimalField(max_digits=20, decimal_places=2, null=True, blank=True)
     valor = models.DecimalField(max_digits=20, decimal_places=2, null=True, blank=True)
-    tipo = models.CharField(max_length=1, choices=TIPOS_TRANSACAO)
+    tipo = models.CharField(max_length=1, choices=TIPOS_TRANSACAO, default='E')
     valor_total = models.DecimalField(max_digits=20, decimal_places=2, null=True, blank=True)
     valor_kg = models.DecimalField(max_digits=20, decimal_places=2, null=True, blank=True)
 
